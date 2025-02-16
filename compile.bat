c6d0914e418f65ca6ec9df689e5e037af2ad751b
Tim C. <timcollins202@gmail.com>
2025-02-16 15:06:58 -0500

    Improve compile script

--- compile.bat
+++ compile.bat
@@ -1,12 +1,23 @@
 @echo off
+@setlocal enabledelayedexpansion
+
+:: Generate a timestamp (YYYYMMDD_HHMMSS)
+for /f "tokens=2 delims==" %%I in ('"wmic os get localdatetime /value"') do set dt=%%I
+set timestamp=%dt:~0,8%_%dt:~8,6%
+
+:: Rename existing dice-nuts.nes if it exists
+if exist output\dice-nuts.nes (
+    rename output\dice-nuts.nes dice-nuts_%timestamp%.nes
+)
+
 @del output\dice-nuts.o
-@del output\dice-nuts.nes
 @del output\dice-nuts.map.txt
 @del output\dice-nuts.labels.txt
 @del output\dice-nuts.nes.ram.nl
 @del output\dice-nuts.nes.0.nl
 @del output\dice-nuts.nes.1.nl
 @del output\dice-nuts.nes.dbg
+
 @echo.
 @echo Compiling...
 \cc65\bin\ca65 dice-nuts.asm -g -o output\dice-nuts.o
@@ -16,19 +27,12 @@
 \cc65\bin\ld65 -o output\dice-nuts.nes -C dice-nuts.cfg output\dice-nuts.o -m output\dice-nuts.map.txt -Ln output\dice-nuts.labels.txt --dbgfile output\dice-nuts.nes.dbg
 @IF ERRORLEVEL 1 GOTO failure
 
-:: Generate a timestamp (YYYYMMDD_HHMMSS)
-for /f "tokens=2 delims==" %%I in ('"wmic os get localdatetime /value"') do set dt=%%I
-set timestamp=%dt:~0,8%_%dt:~8,6%
-
-:: Rename output file with timestamp
-set output_file=dice-nuts_%timestamp%.nes
-rename output\dice-nuts.nes %output_file%
-
 @echo.
-@echo Success! Output saved as %output_file%
+@echo Success. Previous version saved as dice-nuts_%timestamp%.nes
 @GOTO endbuild
 
 :failure
 @echo.
 @echo Build error!
 :endbuild
+@endlocal
